4ddef4594ab1a80e34110bda8a32217237697a1a
voidddd69 <158298019+voidddd69@users.noreply.github.com>
2026-02-03 19:32:00 +0800

    ejdjdn

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     port: 80,
-    mongoDB: "mongodb://localhost:27017/speedsmm", // MongoDB bağlantı adresi
-    siteUrl: "http://localhost", // Site URL Sonuna / koymayın lütfen örn: https://fastuptime.com http://fastuptime.com
+    mongoDB: "mongodb+srv://smm:naACoy36pKyzkmwO@smm.voxnmtc.mongodb.net/", // MongoDB bağlantı adresi
+    siteUrl: "https://speed-smm-v3.vercel.app/", // Site URL Sonuna / koymayın lütfen örn: https://fastuptime.com http://fastuptime.com
     licenseKey: "speedsmm", // Lisans anahtarı bu örnektir. Lütfen kendi lisans anahtarınızı girin. Aksi taktirde sistem kurulumu hatalı olacaktır.
     salt: {
         one: "speedsmm", // MD5 için ilk kurulumdan sonra Şifreleri asla değiştirmeyin! 
